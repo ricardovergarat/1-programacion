1a830c2af4d152e7c45cf09990150bc54dd528cb
ricardovergarat <50001129+ricardovergarat@users.noreply.github.com>
2019-09-29 15:46:37 -0400

    agregamos clases (poo) y encapsulamiento

---
+++ C#/6. Objetos y Clases/empleados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6._Objetos_y_Clases
+{
+    public class Empleados
+    {
+        // contructor
+        // suele ser donde se inicializan los valores de la clase
+        public Empleados()
+        {
+            Nombre = "";
+            sueldo = 0.0m;
+            edad = 0;
+        }
+
+        // atributos
+        public string Nombre;
+        public Decimal sueldo;
+        public int edad;
+
+        // metodos
+        public decimal calcular_saldo(int n_dias)
+        {
+            return sueldo * n_dias;
+        }
+
+    }
+}

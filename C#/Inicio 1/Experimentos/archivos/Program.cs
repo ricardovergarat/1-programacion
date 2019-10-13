28aa07f610ddb549f56f56d6686558c91c29cabf
ricardovergarat <50001129+ricardovergarat@users.noreply.github.com>
2019-10-13 16:35:39 -0400

    se agrego el nuevo sistema de lenguajes

---
+++ C#/Inicio 1/Experimentos/archivos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TextWriter archivo;
+            archivo = new StreamWriter("uno.txt");
+            string mensaje;
+            Console.WriteLine("Ingrese algo:\n");
+            mensaje = Console.ReadLine();
+            archivo.WriteLine(mensaje);
+            archivo.Close();
+            //Console.Clear();
+            
+            
+
+            TextReader leer_archivo;
+            leer_archivo = new StreamReader("uno.txt");
+            Console.WriteLine("tu ingresaste lo sgt:\n");
+            Console.WriteLine(leer_archivo.ReadToEnd());
+            leer_archivo.Close();
+            Console.WriteLine("tambien se creo un archivo de texto llamado uno.txt");
+            Console.WriteLine("el cual tiene escrito lo que acabas de ingresar");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ C#/Proyectos/Registro de gasto/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro_de_gasto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

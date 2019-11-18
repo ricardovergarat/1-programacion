30bc7ccdd4387faf21af91681902503f05553d96
ricardovergarat <50001129+ricardovergarat@users.noreply.github.com>
2019-11-18 06:54:24 -0400

    agregamos nuestra marca de agua o sello distintibo

---
+++ C#/Marca de agua de verdog/izi conosla/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace izi_conosla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("░░░░░░░░░░░░░░░░░░░░░░░░░");
+            Console.WriteLine("░░█▀▀▀░█▀▀▀░░█▀▀░▀▀█░░█░░");
+            Console.WriteLine("░░█░▀█░█░▀█░░█▀▀░▄▀░░░▀░░");
+            Console.WriteLine("░░▀▀▀▀░▀▀▀▀░░▀▀▀░▀▀▀░░▀░░");
+            Console.WriteLine("░░░░░░░░░░░░░░░░░░░░░░░░░");
+            Console.WriteLine("\n");
+            Console.WriteLine("░░░░░░░░░░░░░░░░░░░░░░░░░\n░░█▀▀▀░█▀▀▀░░█▀▀░▀▀█░░█░░\n░░█░▀█░█░▀█░░█▀▀░▄▀░░░▀░░\n░░▀▀▀▀░▀▀▀▀░░▀▀▀░▀▀▀░░▀░░\n░░░░░░░░░░░░░░░░░░░░░░░░░");
+            Console.ReadKey();
+        }
+    }
+}

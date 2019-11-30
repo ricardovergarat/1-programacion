1a20463497c773b6ef86055e91875eede84cb56a
ricardovergarat <50001129+ricardovergarat@users.noreply.github.com>
2019-11-30 14:37:06 -0400

    agregamos fibonachi

---
+++ Python/fibonachi.py
@@ -0,0 +1,23 @@
+
+def pedir_numero():
+	x = input("ingrese que hasta que posicion quiere los nuemros de fibonachi: ")
+	return int(x)
+
+def fibonachi(n):
+	x = 0
+	while x != n:
+		if x == 0:
+			suma = 0
+			ant = suma
+			ant2 = 1
+			print(suma)
+		else:
+			suma = ant + ant2
+			print(suma)
+			ant2 = ant
+			ant = suma
+		x = x + 1
+
+if __name__ == "__main__":
+ 	a = pedir_numero()
+ 	fibonachi(a)

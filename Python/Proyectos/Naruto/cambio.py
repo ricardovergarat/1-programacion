625bd70807308834655137313aca443ef117fa50
ricardovergarat <ricardovergarat1@gmail.com>
2019-12-15 03:28:09 -0400

    se dejo el programa naruto en pausa

--- Python/Proyectos/Naruto/cambio.py
+++ Python/Proyectos/Naruto/cambio.py
@@ -1,40 +1,52 @@
 import os
 
 def listar_archivos(n,lugar):
+	print("estamos en: ",lugar)
 	if n == 0:
 		lista_de_archivos = os.listdir()
-
 	else:
-		lista_de_archivos = os.listdir(lugar)
+		try:
+			lista_de_archivos = os.listdir(lugar)
+		except:
+			print("archivo sin extencion")
 	return lista_de_archivos
 
-def mostrar_lista_de_archivos(lista):
+def mostrar_lista(lista):
 	for x in range(len(lista)):
-		posicion = determinador_de_extenciones(lista[x])
-		elegir_camino(posicion,lista[x])
+		print(lista[x])
+
+def cambiar_lista(lista,directorio):
+	for x in range(len(lista)):
+		carpeta = determinador_de_carpetas(lista[x])
+		if carpeta == "si":
+			sgt_ruta = determinar_siguiente_directorio(directorio,lista[x])
+			sub_archivos = listar_archivos(1,sgt_ruta)
+			archivos = cambiar_lista(sub_archivos,sgt_ruta)
+			lista[x] = archivos
+			print(lista[x])
+
+	return lista
 
 def obtener_ruta():
 	return os.getcwd()
 
-def determinador_de_extenciones(elemento):
+def determinador_de_carpetas(elemento):
 	indice = elemento.find(".")
-	return indice
-
-def elegir_camino(n,elemento):
-	if n == -1:
-		ruta = obtener_ruta()
-		sgt_ruta = determinar_siguiente_directorio(ruta,elemento)
-		archivos = listar_archivos(1,sgt_ruta)
-		print(archivos)
+	if indice == -1:
+		return "si"
 	else:
-		print(elemento)
+		return "no"
+
 
 def determinar_siguiente_directorio(directorio_actual,carpeta):
 	nuevo_directorio = directorio_actual + "\\" + carpeta
 	return nuevo_directorio
 
 archivos = listar_archivos(0,"")
-mostrar_lista_de_archivos(archivos)
+ruta = obtener_ruta()
+archivos = cambiar_lista(archivos,ruta)
+mostrar_lista(archivos)
+
 
 
 

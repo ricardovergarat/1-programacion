6ca2f0cf0e489c7d2e6a6ee7387d09a2d06699ef
ricardovergarat <ricardovergarat1@gmail.com>
2020-03-23 19:59:02 -0300

    agregamos pilas en c++ y un problema en pila en c

--- C++/Estructuras de datos/pilas.cpp
+++ C++/Estructuras de datos/pilas.cpp
@@ -5,33 +5,76 @@ using namespace std;
 
 struct nodo{
 	int dato;
-	nodo *sgt; // ubicacion del ssiguietne elemento
+	nodo *sgt; // ubicacion del siguietne elemento
 };
 
-void mostrar_pila(nodo *&pila){
-	cout << "El dato es: " << pila -> dato << endl;
-	cout << "apuntador anterior es: " << pila -> sgt << endl;
-	cout << pila -> sgt << endl;
-	nodo *anterior = pila -> sgt;
-	//cout << "Esto es anterior: " << anterior << endl;
-	
+void mostrar_pila(nodo *pila){ // el elemento mas a la izquierda es el tope de la pila
+	nodo *pila_actual = pila;
+	if ( pila_actual == NULL ){
+		cout << "[]" << endl;
+	}else{
+		cout << "[";
+		while ( pila_actual -> sgt != NULL ){
+			cout << pila_actual -> dato << ",";
+			pila_actual = pila_actual -> sgt;
+		}
+	cout << "]" << endl;
+	}
 }
 
 void agregar_elemento(nodo *&pila, int numero){
 	nodo *nuevo_espacio = new nodo(); // reservamos espacio en la memoria
 	nuevo_espacio -> dato = numero; // inicializamos el elemento que queriamos poner en la pila
-	nuevo_espacio -> sgt = pila; // esto el elemento que esta debajo por que es una pila, es decir nuevo espacio es ahora la cima
-	pila = nuevo_espacio; // por que nuestra nueva pila ahora le agregamos un nuevo elemento
+	nuevo_espacio -> sgt = pila; // ubicacion de la pila anterior
+	//cout << "Elemento: " << nuevo_espacio -> dato << "		apuntador anterior: " << nuevo_espacio -> sgt << endl;
+	pila = nuevo_espacio; // por que nuestra nueva pila ahora le agregamos un nuevo elemento, es decir actualizamo la pila
 }
 
+void quitar_elemento(nodo *&pila){
+	cout << "El elemento a quitar es: " << pila -> dato << "	ubicacion de la pila anterior es: " << pila -> sgt << endl;
+	pila = pila -> sgt;
+}
+
+void obtener_tope(nodo *pila){// cambiar a tipo de retorno si queremos el tope en una variable
+	if ( pila == NULL ){
+		cout << "Esta pila esta vacia" << endl;
+		// return 0; // esto por si queremos retornar el tope
+	}
+	// tipo tope = pila -> dato;
+	int tope = pila -> dato;
+	cout << "Esto es el tope: " << tope << endl;
+}
+
+void obtener_largo(nodo *pila){
+	if ( pila == NULL ){
+		cout << "0" << endl;
+		//return 0;
+	}
+	int x = 0;
+	nodo *pila_actual = pila;
+	while ( pila_actual -> sgt != NULL ){
+		x = x + 1;
+		pila_actual = pila_actual -> sgt;
+	}
+	cout << "La pila tiene largo: " << x << endl;
+}
 
 int main(){
 	nodo *pila = NULL; // pila = []
+	mostrar_pila(pila);
 	agregar_elemento(pila,5);
-	mostrar_pila(pila);
 	agregar_elemento(pila,3);
-	mostrar_pila(pila);
 	agregar_elemento(pila,8);
 	mostrar_pila(pila);
+	agregar_elemento(pila,13);
+	agregar_elemento(pila,17);
+	agregar_elemento(pila,19);
+	agregar_elemento(pila,27);
+	agregar_elemento(pila,65);
+	agregar_elemento(pila,132);
+	mostrar_pila(pila);
+	quitar_elemento(pila);
+	obtener_tope(pila);
+	obtener_largo(pila);
 	return 0;
 }
--- C/Curso/Estrucuras/pila.c
+++ C/Curso/Estrucuras/pila.c
@@ -1,19 +1,40 @@
 # include <stdlib.h>
 
-struct nodo{
+typedef struct nodo{
 	int dato;
 	struct nodo *sgt; // aqui guardamos donde esta el siguiente elemento
-};
-
-struct nodo *cabeza = NULL;
-
-void insertar_nodo(int a){
-	struct nodo *nuevo_espacio = (nodo*) malloc(sizeof(nodo));
-	nuevo_espacio -> a;
-	
+}nodo;
+/*
+void mostrar_pila(nodo *pila){
+	nodo *pila_actual = pila;
+	if (pila_actual == NULL){
+		printf("[]\n");
+	}else{
+		printf("[");
+		while (pila_actual -> sgt != NULL){
+			printf("%i,",pila_actual -> dato);
+			pila_actual = pila_actual -> sgt;
+		}
+		printf("]\n");
+	}
+}
+*/
+void agregar_elemento(nodo *pila, int a){
+	nodo *nuevo_espacio = (nodo*) malloc(sizeof(nodo)); // reservamos espacio en memoria de tamaño nodo(su tamaño en bytes)
+	nuevo_espacio -> dato = a;
+	nuevo_espacio -> sgt = pila;
+	pila = nuevo_espacio;
+	printf("El dato es: %i     ubicacion pila anterior: %i\n",nuevo_espacio -> dato,nuevo_espacio -> sgt);
 }
 
-int main(){
 
+int main(){
+	nodo *pila = NULL;
+	agregar_elemento(&pila,3);
+	agregar_elemento(&pila,4);
+	agregar_elemento(&pila,42);
+	agregar_elemento(&pila,443);
+	agregar_elemento(&pila,123);
+	//mostrar_pila(pila);
 	return 0;
 }

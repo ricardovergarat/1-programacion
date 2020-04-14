bec8f04901459de39e4355aa14c6a752346bbc0c
ricardovergarat <ricardovergarat1@gmail.com>
2020-04-14 12:24:33 -0400

    prototipo programa de dark sousl

--- Zona/orden_numeros.py
+++ Zona/orden_numeros.py
@@ -12,7 +12,6 @@ def ordenar(lista):
         ordenado = esta_ordenada(lista)
     return lista
 
-
 def esta_ordenada(lista):
     x = 0
     while x != len(lista) - 1:
@@ -21,9 +20,7 @@ def esta_ordenada(lista):
         x = x + 1
     return True
 
-
 if __name__ == "__main__":
-    print("starts")
     n = [32,534,35453,3123,4,13,65,9999,3123]
     print(n)
     n = ordenar(n)
